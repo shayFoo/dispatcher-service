044b37656e06756e9bbdedcd0e151690f86045c1
shyFoo <oasis1975sun@gmail.com>
2025-10-05 17:50:26 +0900

    feat: add Kubernetes deployment and service configuration for dispatcher service

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ extra["springCloudVersion"] = "2025.0.0"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.cloud:spring-cloud-stream-binder-rabbit")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
     testImplementation("org.springframework.cloud:spring-cloud-stream-test-binder")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
